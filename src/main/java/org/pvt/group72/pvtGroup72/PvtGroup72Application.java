6b86cb0343f199c8445cd2939bb27c2e34e10861
sun <empaemanuel@gmail.com>
2020-04-21 14:24:46 +0200

    added dependency

--- src/main/java/org/pvt/group72/pvtGroup72/PvtGroup72Application.java
+++ src/main/java/org/pvt/group72/pvtGroup72/PvtGroup72Application.java
@@ -2,13 +2,14 @@ package org.pvt.group72.pvtGroup72;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class PvtGroup72Application {
+public class PvtGroup72Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PvtGroup72Application.class, args);
---
+++ src/main/java/org/pvt/group72/pvtGroup72/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.pvt.group72.pvtGroup72;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PvtGroup72Application.class);
+	}
+
+}
