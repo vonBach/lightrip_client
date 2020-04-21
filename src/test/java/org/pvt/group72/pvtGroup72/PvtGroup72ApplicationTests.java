bc6ccf5b16116be6078734c579769e77e12090d4
sun <empaemanuel@gmail.com>
2020-04-21 12:54:58 +0200

    init

---
+++ src/test/java/org/pvt/group72/pvtGroup72/PvtGroup72ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pvt.group72.pvtGroup72;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvtGroup72ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
