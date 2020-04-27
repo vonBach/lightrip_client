237b7ab1c63bce2d073e097eef4fb445622da213
sun <empaemanuel@gmail.com>
2020-04-27 12:26:04 +0200

    init w. flutter support

---
+++ client/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:client/view/start_view.dart';
+
+
+//Entry point hands over control to the controller
+void main() => runApp(StartApp());
+
+class StartApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StartView(),
+    );
+  }
+}
---
+++ server/src/main/java/org/group72/server/ServerApplication.java
@@ -0,0 +1,23 @@
+package org.group72.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ServerApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name){
+		return String.format("Helo %s!", name);
+	}
+
+}
