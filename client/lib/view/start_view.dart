be9bdb315e12052898ce85b6c43c41a95f2be27b
martinB <martin.bjersten@gmail.com>
2020-04-29 13:17:54 +0200

    Added google maps

--- client/lib/view/start_view.dart
+++ client/lib/view/start_view.dart
@@ -1,51 +1,100 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 
-class StartView extends StatefulWidget{
-
+class StartView extends StatefulWidget {
   @override
   _StartViewState createState() => _StartViewState();
 }
 
 class _StartViewState extends State<StartView> {
+  Completer<GoogleMapController> _controller = Completer();
   Future<http.Response> futureGreeting;
 
-  void _greetServer(){
+  void _greetServer() {
     setState(() {
       futureGreeting = _getGreeting();
     });
   }
 
-  Future<http.Response> _getGreeting() async{
+  Future<http.Response> _getGreeting() async {
     print('sending request to server');
-    http.Response r = await http.get('https://group2-75.pvt.dsv.su.se/hello?name=group72');
+    http.Response r =
+        await http.get('https://group2-75.pvt.dsv.su.se/hello?name=group72');
     print('${r.body}');
     return r;
   }
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
+    return Scaffold(
         body: Center(
           child: FutureBuilder(
-            future: futureGreeting,
-            builder: (context, snapshot){
-              var _message;
-              if(snapshot.hasData){
-                _message = snapshot.data.body;
-              } else {
-                _message = "Press the button to greet server";
-              }
-              return Text(_message,style: TextStyle(fontSize: 26));
-            }
-          ),
+              future: futureGreeting,
+              builder: (context, snapshot) {
+                var _message;
+                if (snapshot.hasData) {
+                  _message = snapshot.data.body;
+                } else {
+                  _message = "Press the button to greet server";
+                }
+                return Text(_message, style: TextStyle(fontSize: 26));
+              }),
+        ),
+        floatingActionButton: Stack(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 31),
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: FloatingActionButton(
+                  onPressed: () {
+                    mapPage(context);
+                  },
+                  child: Icon(Icons.map),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: FloatingActionButton(
+                heroTag: "button2",
+                onPressed: _greetServer,
+                child: Icon(Icons.autorenew),
+              ),
+            ),
+          ],
+        ));
+  }
 
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.autorenew),
-          onPressed: _greetServer,
-        ),
-      );
+  Widget _googleMap(BuildContext context) {
+    return Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition:
+              CameraPosition(target: LatLng(59.3293, 18.0686), zoom: 12),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ));
+  }
+
+  void mapPage(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
+      return Scaffold(
+          appBar: AppBar(
+            title: const Text('Map'),
+          ),
+          body: Center(
+            child: Stack(
+              children: <Widget>[_googleMap(context)],
+            ),
+          ));
+    }));
   }
 }
