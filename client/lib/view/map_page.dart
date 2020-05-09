4342e32c2c352b26b1f2ac7af634b823f2e0b7d7
sun <empaemanuel@gmail.com>
2020-05-09 20:04:43 +0200

    tog bort ett överflödigt navigeringshopp

--- client/lib/view/map_page.dart
+++ client/lib/view/map_page.dart
@@ -11,7 +11,6 @@ import 'package:client/view/facebookLogin.dart';
 import 'package:client/view/start_view.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
- 
 
 class Route {
   double latA;
@@ -20,78 +19,72 @@ class Route {
   double longB;
 
   Route({this.latA, this.longA, this.latB, this.longB});
-
 }
 
-class MapPage extends StatefulWidget{
+class MapPage extends StatefulWidget {
   @override
   _MapPageState createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> {
-PermissionStatus _status;
-final Set<Polyline> polyline = {};
-GoogleMapController _controller;
-GoogleMapPolyline googleMapPolyline = new GoogleMapPolyline(apiKey: 'KEY HERE, DIRECTIONS API');
-List routeList;
-List<LatLng> routeCoordsList = new List<LatLng>();
-int distance;
-String user;
+  PermissionStatus _status;
+  final Set<Polyline> polyline = {};
+  GoogleMapController _controller;
+  GoogleMapPolyline googleMapPolyline =
+      new GoogleMapPolyline(apiKey: 'KEY HERE, DIRECTIONS API');
+  List routeList;
+  List<LatLng> routeCoordsList = new List<LatLng>();
+  int distance;
+  String user;
 
-@override
-void initState(){
-  super.initState();
-  getData();
-}
-
-void getData() async {
-  var permissions = await Permission.location.request();
- 
-  if (permissions == PermissionStatus.granted) {
-    // Either the permission was already granted before or the user just granted it.
-
-    final response = await http.get('https://group2-75.pvt.dsv.su.se/route/demo');
-
-    if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
-      Map dataMap = json.decode(response.body);
-      routeList = dataMap['route'];
-      distance = dataMap['distance'];
-      user = dataMap['user'];
-      int index = 0;
-      for(dynamic coords in routeList){
-        LatLng lineCoords = new LatLng(coords['latitude'], coords['longitude']);
-        routeCoordsList.add(lineCoords);
-      }
-      
-      //dataMap['route']
-    } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to load album');
-    }
-  }else{
-      await Permission.location.request();
+  @override
+  void initState() {
+    super.initState();
+    //getData();
   }
-}
-  
 
+  void getData() async {
+    var permissions = await Permission.location.request();
 
- Widget _googleMap(BuildContext context) {
+    if (permissions == PermissionStatus.granted) {
+      // Either the permission was already granted before or the user just granted it.
+
+      final response =
+          await http.get('https://group2-75.pvt.dsv.su.se/route/demo');
+
+      if (response.statusCode == 200) {
+        // If the server did return a 200 OK response,
+        // then parse the JSON.
+        Map dataMap = json.decode(response.body);
+        routeList = dataMap['route'];
+        distance = dataMap['distance'];
+        user = dataMap['user'];
+        int index = 0;
+        for (dynamic coords in routeList) {
+          LatLng lineCoords =
+              new LatLng(coords['latitude'], coords['longitude']);
+          routeCoordsList.add(lineCoords);
+        }
+
+        //dataMap['route']
+      } else {
+        // If the server did not return a 200 OK response,
+        // then throw an exception.
+        throw Exception('Failed to load album');
+      }
+    } else {
+      await Permission.location.request();
+    }
+  }
+
+  Widget _googleMap(BuildContext context) {
     return Container(
-        height: MediaQuery
-            .of(context)
-            .size
-            .height,
-        width: MediaQuery
-            .of(context)
-            .size
-            .width,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
         child: GoogleMap(
           mapType: MapType.normal,
           initialCameraPosition:
-          CameraPosition(target: LatLng(59.3293, 18.0686), zoom: 12),
+              CameraPosition(target: LatLng(59.3293, 18.0686), zoom: 12),
           onMapCreated: onMapCreated,
           polylines: polyline,
         ));
@@ -108,19 +101,17 @@ void getData() async {
         width: 4,
         color: Colors.blue,
         startCap: Cap.roundCap,
-        endCap: Cap.buttCap, 
-        ));
-
+        endCap: Cap.buttCap,
+      ));
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
-      return Scaffold(
-          appBar: AppBar(
-            title: const Text('Map'),
-            actions: <Widget>[
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Map'),
+          actions: <Widget>[
             IconButton(
                 icon: Icon(Icons.settings),
                 tooltip: 'Fetch data',
@@ -128,14 +119,14 @@ void getData() async {
                   //do something
                 })
           ],
-          ),
-          body: Center(
-            child: 
-            Center(
+        ),
+        body: Center(
+            child: Center(
                 child: Stack(
-              children: <Widget>[_googleMap(context)],))
-            
-          ));
-    }));
+          children: <Widget>[
+            Text("test"),
+            _googleMap(context)
+          ],
+        ))));
   }
 }
