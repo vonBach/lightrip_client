b05bfd86701005e64efd3d64b6aa36355edc6925
Max Lindahl <Max.Lindahl97@gmail.com>
2020-05-09 23:26:17 +0200

    cleaned up, added comments, uncommented getData

--- client/lib/view/map_page.dart
+++ client/lib/view/map_page.dart
@@ -1,25 +1,11 @@
 import 'dart:convert';
-import 'dart:async';
 
 import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:google_map_polyline/google_map_polyline.dart';
 import 'package:http/http.dart' as http;
-import 'package:client/view/facebookLogin.dart';
-import 'package:client/view/start_view.dart';
-import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Route {
-  double latA;
-  double longA;
-  double latB;
-  double longB;
-
-  Route({this.latA, this.longA, this.latB, this.longB});
-}
 
 class MapPage extends StatefulWidget {
   @override
@@ -27,43 +13,40 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  PermissionStatus _status;
-  final Set<Polyline> polyline = {};
-  GoogleMapController _controller;
-  GoogleMapPolyline googleMapPolyline =
-      new GoogleMapPolyline(apiKey: 'KEY HERE, DIRECTIONS API');
-  List routeList;
-  List<LatLng> routeCoordsList = new List<LatLng>();
-  int distance;
-  String user;
+  final Set<Polyline> polyline = {};     //set containing the polylines to be drawn on map
+  GoogleMapController _controller;       //controller for the map
+  List routeList;                        //initial list to handle first objects being sent from server
+  List<LatLng> routeCoordsList = new List<LatLng>(); //second list to handle the transformed nodes
+  int distance;     //distance of route
+  String user;      //user
 
   @override
-  void initState() {
+  void initState() {  //called once in the beginning
     super.initState();
-    //getData();
+    getData();  //get data needed for lines
   }
 
   void getData() async {
-    var permissions = await Permission.location.request();
+    var permissions = await Permission.location.request();  //ask for permission to use location
 
-    if (permissions == PermissionStatus.granted) {
+    if (permissions == PermissionStatus.granted) {  //if its granted do this
       // Either the permission was already granted before or the user just granted it.
 
       final response =
-          await http.get('https://group2-75.pvt.dsv.su.se/route/demo');
+          await http.get('https://group2-75.pvt.dsv.su.se/route/demo');  //ask server for data
 
-      if (response.statusCode == 200) {
+      if (response.statusCode == 200) {  //if request succeeded
         // If the server did return a 200 OK response,
         // then parse the JSON.
-        Map dataMap = json.decode(response.body);
-        routeList = dataMap['route'];
-        distance = dataMap['distance'];
-        user = dataMap['user'];
-        int index = 0;
-        for (dynamic coords in routeList) {
-          LatLng lineCoords =
-              new LatLng(coords['latitude'], coords['longitude']);
-          routeCoordsList.add(lineCoords);
+        Map dataMap = json.decode(response.body);   //turn response into a readable map      ---      could probably simplify this part slightly
+        routeList = dataMap['route'];               //save the list from the map to a separate list                                            |
+        distance = dataMap['distance'];             //save the distance                                                                        |
+        user = dataMap['user'];                     //save the user                                                                            |
+                                                    //                                                                                         |
+        for (dynamic coords in routeList) {                         //iterate through the list of nodes, nodes are saved as map objects        |
+          LatLng lineCoords =                                       //save the lat and long values as a LatLng object                          |
+              new LatLng(coords['latitude'], coords['longitude']);  //                                                                         |
+          routeCoordsList.add(lineCoords);                          //add the LatLng obj to a new list                                         |
         }
 
         //dataMap['route']
@@ -73,11 +56,11 @@ class _MapPageState extends State<MapPage> {
         throw Exception('Failed to load album');
       }
     } else {
-      await Permission.location.request();
+      await Permission.location.request();  //not sure if this is needed
     }
   }
 
-  Widget _googleMap(BuildContext context) {
+  Widget _googleMap(BuildContext context) {  //create the google map object
     return Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
@@ -85,19 +68,19 @@ class _MapPageState extends State<MapPage> {
           mapType: MapType.normal,
           initialCameraPosition:
               CameraPosition(target: LatLng(59.3293, 18.0686), zoom: 12),
-          onMapCreated: onMapCreated,
-          polylines: polyline,
+          onMapCreated: onMapCreated,  //call onMapCreated func when created
+          polylines: polyline,          //add the swigglies put in the set polyline in func above to the map
         ));
   }
 
-  void onMapCreated(GoogleMapController controller) {
+  void onMapCreated(GoogleMapController controller) {  //called after the map is created
     setState(() {
       _controller = controller;
 
-      polyline.add(Polyline(
+      polyline.add(Polyline(              //add the blue swiggly lines to a set of <Polyline>
         polylineId: PolylineId('route1'),
         visible: true,
-        points: routeCoordsList,
+        points: routeCoordsList,       //takes a list of <LatLng>
         width: 4,
         color: Colors.blue,
         startCap: Cap.roundCap,
@@ -112,11 +95,11 @@ class _MapPageState extends State<MapPage> {
         appBar: AppBar(
           title: const Text('Map'),
           actions: <Widget>[
-            IconButton(
+            IconButton(                         //this is the settings button in the top right
                 icon: Icon(Icons.settings),
                 tooltip: 'Fetch data',
                 onPressed: () {
-                  //do something
+                  //do something          does nothing atm
                 })
           ],
         ),
@@ -125,8 +108,11 @@ class _MapPageState extends State<MapPage> {
                 child: Stack(
           children: <Widget>[
             Text("test"),
-            _googleMap(context)
+            _googleMap(context)  //this is where we add our map object making it visible in the interface
           ],
-        ))));
+        )
+      )
+    )
+    );
   }
 }
