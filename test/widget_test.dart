9e668c4c5336f50a0cf2438951d0e68deded5c1c
sun <empaemanuel@gmail.com>
2020-05-18 01:19:30 +0200

    added empty test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,9 +11,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:client/main.dart';
 
 void main() {
-//  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//    // Build our app and trigger a frame.
-//    await tester.pumpWidget(StartApp());
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    //await tester.pumpWidget(StartApp());
 //
 //    // Verify that our counter starts at 0.
 //    expect(find.text('0'), findsOneWidget);
@@ -26,5 +26,5 @@ void main() {
 //    // Verify that our counter has incremented.
 //    expect(find.text('0'), findsNothing);
 //    expect(find.text('1'), findsOneWidget);
-//  });
+  });
 }
