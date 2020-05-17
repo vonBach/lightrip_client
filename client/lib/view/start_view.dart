1c20a136a8071bcc6fecfb905be52e11046266ca
mpx9411 <56627018+mpx9411@users.noreply.github.com>
2020-05-17 18:01:07 +0200

    merged services into ui

--- client/lib/view/start_view.dart
+++ client/lib/view/start_view.dart
@@ -1,21 +1,10 @@
-
-import 'dart:async';
-
-import 'package:client/services/googlemap_service.dart';
-
 import 'package:flutter/cupertino.dart';
 import 'package:client/view/sign_in_view.dart';
 import 'package:flutter/material.dart';
-import 'package:client/view/facebookLogin.dart';
-import 'package:client/view/login_page.dart';
-import 'package:http/http.dart' as http;
-
 import 'package:client/view/email_view.dart';
 import 'package:client/widgets/navigationButton_widget.dart';
 import 'package:client/view/map_page.dart';
 import 'package:client/custom_color.dart';
-
-import '../service_locator.dart';
 
 class StartView extends StatefulWidget {
   @override
@@ -23,11 +12,6 @@ class StartView extends StatefulWidget {
 }
 
 class _StartViewState extends State<StartView> {
-
-  var googleMapService = locator.get<GoogleMapService>();
-  //Completer<GoogleMapController> _controller = Completer();
-  Future<http.Response> futureGreeting;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -111,58 +95,6 @@ class _StartViewState extends State<StartView> {
             ],
           )
         ]));
-  }
-
-    /*Widget _googleMap(BuildContext context) {
-      return Container(
-          height: MediaQuery
-              .of(context)
-              .size
-              .height,
-          width: MediaQuery
-              .of(context)
-              .size
-              .width,
-          child: GoogleMap(
-            mapType: MapType.normal,
-            initialCameraPosition:
-            CameraPosition(target: LatLng(59.3293, 18.0686), zoom: 12),
-            onMapCreated: (GoogleMapController controller) {
-              _controller.complete(controller);
-            },
-          ));
-    }*/
-
-  void mapPage(BuildContext context) {
-    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
-      return Scaffold(
-          appBar: AppBar(
-            title: const Text('Map'),
-          ),
-          body: Center(
-            child: Stack(
-              children: <Widget>[googleMapService.googleMap(context)],
-            ),
-          ));
-    }));
-  }
-
-
-  void googlePage(BuildContext context) {
-    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
-      return GooglePage();
-    }));
-              Row(children: <Widget>[
-                Text('Already have an account? '),
-                InkWell(
-                    child: Text('Sign in',
-                        style: TextStyle(
-                            color: Colors.red,
-                            decoration: TextDecoration.underline)),
-                    onTap: () {
-                      signInPage(context);
-                    })
-              ]);
   }
 }
 
